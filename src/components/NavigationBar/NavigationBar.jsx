8ad665c4c498fbf1514fddf8653f1e03bb1f1adb
mahmoudramadan0020 <mahmoudramadan0040@gmail.com>
2023-05-30 22:23:55 +0300

    intial react project

---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,15 @@
+function NavigationBar() {
+    return ( 
+        <div>
+            <div class="main-header">
+                <div class="logo"></div>
+                <div class="search-product"></div>
+                <div class="account-section">
+                <i class="bi bi-0-square"></i>
+                </div>
+            </div>
+        </div> 
+    );
+}
+
+export default NavigationBar;
